ddf8368e1c374ba11f3831cbd9835734b29c98a2
Jair Brann Junior <jairjunior02@hotmail.com>
2021-04-22 23:10:52 -0300

    Creating player and SSG dynamic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from './home.module.scss';
 
@@ -25,6 +27,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+    const { play } = useContext(PlayerContext);
+
     return (
         <div className={styles.homepage}>
             <section className={styles.latestEpisodes}>
@@ -49,7 +53,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                                     <span>{episode.durationAsString}</span>
                                 </div>
                                 
-                                <button type="button">
+                                <button type="button" onClick={() => play(episode)}>
                                     <img src="/play-green.svg" alt="Tocar episódio" />  
                                 </button>
                             </li>
